8cd7862f37affde6fdcb30e620e5aa1039aab038
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-08-26 17:19:32 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,8 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
+  board = board
+  index = index
  if position_taken(board, index) == true || index > 8
    false
   elsif position_taken(board, index) == false
