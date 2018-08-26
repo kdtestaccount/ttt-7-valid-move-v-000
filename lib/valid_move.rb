b58edcb184a274da5f5693bfc0afd637f92f39fb
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-08-26 17:12:03 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 
-def valid_move(board, index, value)
+def valid_move?(board, index, value)
  if position_taken == true
    puts "This move can't be completed"
    true
@@ -12,7 +12,7 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-def position_taken(board, index)
+def position_taken?(board, index)
   if board[index] == "" || board[index] == " " || board[index] == "  " || board[index] == nil
     false 
   elsif board[index] == "X" || board[index] == "O"
