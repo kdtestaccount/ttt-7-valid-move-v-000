5b01ed9a838169ce8cae1e793cecb063dd400c71
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-08-26 17:11:06 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,6 +5,7 @@ def valid_move(board, index, value)
    puts "This move can't be completed"
   else
     board[index] = value
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
